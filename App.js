c7bc9debd148ff79eb57c7dc6677268ca219c417
adi604 <84729170+adi604@users.noreply.github.com>
2023-01-08 22:24:00 +0200

    update design

--- App.js
+++ App.js
@@ -15,7 +15,7 @@ const Stack = createNativeStackNavigator();
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Oueyou" component={HomeScreen} />
         <Stack.Screen name="LoginScreen" component={LoginScreen} />
         <Stack.Screen name="SignUpScreen" component={SignUpScreen} />
