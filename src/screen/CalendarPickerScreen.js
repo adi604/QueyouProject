ba35d5f4565bb6577be53553c58c8b01c19a15f9
Ben Levi <benlevi7770528@gmail.com>
2023-03-18 17:55:24 +0200

    calendar and times.

--- src/screen/CalendarPickerScreen.js
+++ src/screen/CalendarPickerScreen.js
@@ -1,16 +1,44 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, ImageBackground } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, ImageBackground, Platform, Button} from 'react-native';
 import CalendarPicker from 'react-native-calendar-picker';
 import { LinearGradient } from 'expo-linear-gradient';
+import DateTimePicker from '@react-native-community/datetimepicker';
+
 
 
 const CalendarPickerScreen = props => {
-
     const [selectedDate, setSelectedDate] = useState("");
+
+    const [date, setDate] = useState(new Date());
+    const [mode, setMode] = useState('time');
+    const [show, setShow] = useState(false);
+    const [text, setText] = useState('');
 
     const onDateChangeFunc = (date) => {
         setSelectedDate(date.toString());
     };
+
+    const onChangeTime = (event, selectedDate) => {
+        const currentDate = selectedDate || date;
+        setShow(Platform.OS === 'ios');
+        setDate(currentDate);
+        let tempDate = new Date(currentDate)
+        let c1 = '';
+        if (tempDate.getHours() < 10) {
+            c1 = '0';
+        }
+        let c2 = '';
+        if (tempDate.getMinutes() < 10) {
+            c2 = '0';
+        }
+        let fTime = c1 + tempDate.getHours() + ':' + c2 + tempDate.getMinutes();
+        setText(fTime)
+    }
+    const showMode = (currentMode) => {
+        setShow(true);
+        setMode(currentMode);
+    }
+
 
     return (
         <View style={styles.container}>
@@ -35,8 +63,25 @@ const CalendarPickerScreen = props => {
 
             </LinearGradient>
             
+            
             <View>
-                <Text style={styles.dateStr}>SELECTED : { selectedDate }</Text>
+                <Text style={styles.dateStr}>Date : { selectedDate }</Text>
+
+                    <View style={{margin:30, width:150, left:100, top:8}}>
+                        <Button title='Time' onPress={()=>{showMode('time')}}/>
+                    </View>
+                    {show && (
+                        <DateTimePicker
+                        testID='dateTimePicker'
+                        value={date}
+                        mode={mode}
+                        is24Hour={true}
+                        display='default'
+                        onChange={onChangeTime}
+                        />
+                    )}
+                    <Text style={styles.timeStr}>Time : {text}</Text>
+                
                 <TouchableOpacity style={styles.viButton}>
                     <ImageBackground source={require('../../assets/vi2.png')} style={styles.vi}>
                         <Text></Text>
@@ -45,7 +90,7 @@ const CalendarPickerScreen = props => {
             </View>
 
         </View>
-    );
+    )
 
 }
 const styles = StyleSheet.create({
@@ -68,14 +113,19 @@ const styles = StyleSheet.create({
 },
 vi: {
     height: 70,
-    top: 50
 },
 dateStr: {
     left: 10,
-    top: 30,
+    top: 5,
     fontSize: 17,
     fontWeight: "bold",
 },
+timeStr: {
+    left: 10,
+    fontSize: 17,
+    fontWeight: "bold",
+},
+
 });
 
 export default CalendarPickerScreen
