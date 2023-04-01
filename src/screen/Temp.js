06abb710e015c9a9d50c822df8dbe9b2c2198e7b
adi604 <84729170+adi604@users.noreply.github.com>
2023-04-01 19:00:58 +0300

    try to change nevidator

--- src/screen/Temp.js
+++ src/screen/Temp.js
@@ -2,6 +2,7 @@ import React from "react";
 import {  createDrawerNavigator, IconComponent } from "@react-navigation/drawer";
 import {DrawerActions} from '@react-navigation/native';
 import { Icon, ScrollView, StyleSheet, Text, View, ImageBackground, Image, TouchableOpacity, Alert, TextInput } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import SearchUserScreen from './SearchUserScreen'
 import MyAppointment from '../components/MyAppointment'
@@ -10,14 +11,17 @@ import Settings from './Settings'
 
 const Drawer = createDrawerNavigator();
 
+const Tab = createBottomTabNavigator();
+
+
 const Temp = props => {
   return (
-    <Drawer.Navigator initialRouteName='SearchUserScreen'>
-      <Drawer.Screen name="Search" component={SearchUserScreen} options={{headerShown:false}}/>
-      <Drawer.Screen name="My Appointments" component={MyAppointment} options={{headerShown:false}}/>
-      <Drawer.Screen name="Reviews" component={Reviews} options={{headerShown:true}}/>
-      <Drawer.Screen name="Settings" component={Settings} options={{headerShown:false, headerStyle: {backgroundColor: '#e7305b'}, headerTintColor: '#FFF', headerTitleStyle: {fontSize: 20, letterSpacing: 0.2,}}}/>
-    </Drawer.Navigator>
+    <Tab.Navigator initialRouteName='SearchUserScreen'>
+      <Tab.Screen name="Search" component={SearchUserScreen} options={{headerShown:false}}/>
+      <Tab.Screen name="My Appointments" component={MyAppointment} options={{headerShown:false}}/>
+      <Tab.Screen name="Reviews" component={Reviews} options={{headerShown:true}}/>
+      <Tab.Screen name="Settings" component={Settings} options={{headerShown:false, headerStyle: {backgroundColor: '#e7305b'}, headerTintColor: '#FFF', headerTitleStyle: {fontSize: 20, letterSpacing: 0.2,}}}/>
+    </Tab.Navigator>
   )
 }
 
