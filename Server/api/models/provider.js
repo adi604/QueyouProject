d321e886416e15d5e50a0e61d6e37df6d08cb7c8
Ben Levi <benlevi7770528@gmail.com>
2023-04-15 13:02:15 +0300

    fix signup provider, and members of provider model

--- Server/api/models/provider.js
+++ Server/api/models/provider.js
@@ -14,8 +14,8 @@ const providerSchema = mongoose.Schema({
     durationMeeting: {type: Number, required: true}, // in minutes. example : 25.
     openTime: {type: String, required: true}, // example : "08:25".
     closeTime: {type: String, required: true}, // example : "20:00".
-    disabledDays: {type: [Number], required: true}, // list of indexes between 0 to 6. example : [1, 3].
-    disabledDates: {type: [String], required: true} // example : ['2023-04-24', '2023-04-25'].
+    disabledDays: {type: [Number], required: false, default: []}, // list of indexes between 0 to 6. example : [1, 3].
+    disabledDates: {type: [String], required: false, default: []} // example : ["2023-04-24", "2023-04-25"].
 });
 
 module.exports = mongoose.model('Provider', providerSchema);
