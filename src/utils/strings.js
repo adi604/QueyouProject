32eedffc870c97a1367a5aa596d1504211a90431
guybenr <guybenr@hotmail.com>
2023-06-04 15:43:09 +0300

    add last name and first name on customer

--- src/utils/strings.js
+++ src/utils/strings.js
@@ -5,3 +5,5 @@ export const INVALID_LOGIN_MSG = "Invalid username or password, please try again
 export const REPEAT_PASSWORD_MSG = "Passwords do not match.";
 export const GOOGLE_PLACES_API_KEY = "AIzaSyDLU7ME7Ck5nykTBrfNidv-w1-Uua-0dUM";
 export const GOOGLE_PLACES_DETAILS_URL = "https://maps.googleapis.com/maps/api/place/details/json?place_id=";
+export const LAST_NAME_MSG = "Last name is required.";
+export const FIRST_NAME_MSG = "First name is required.";
